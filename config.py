71969ed4ba71bf47d5c572f1d8bbe8756f8188b2
Amit Bothra <amit.bothra@outlook.com>
2020-02-16 15:03:34 -0600

    added data model and configured database

--- config.py
+++ config.py
@@ -2,4 +2,7 @@ import os
 
 class Config(object):
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+  # Database URI
+  SQLALCHEMY_DATABASE_URI = 'postgres+pyscopg2://postgres:postgres@localhost:5432/learnworld'
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
   
